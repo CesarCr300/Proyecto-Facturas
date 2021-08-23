1c08b308884f90ce5ad26918b88f7a254cd6965e
CesarCr300 <cc30122003@gmail.com>
2021-08-23 18:10:28 -0500

    refactorización-conseguir_datos

--- base.py
+++ base.py
@@ -1,8 +1,13 @@
 from xml.dom import minidom
 import os
 import pandas
+
 def obtener_lista_archivos(origen):
     return os.listdir(facturasOrigen)
+
+def conseguir_dato_segun_etiqueta(doc, direccionFactura, etiqueta,numero_etiqueta):
+    dato = doc.getElementsByTagName(etiqueta)[numero_etiqueta]
+    return dato.firstChild.data
 
 facturasOrigen = "facturas"
 facturas = obtener_lista_archivos(facturasOrigen)  #listaDeArchivos
@@ -17,21 +22,19 @@ kilogramos = []
 montoLoop = 0
 montoMensual = 0
 for factura in facturas:
-    directionFactura = facturasOrigen + "/" + factura
+    direccionFactura = facturasOrigen + "/" + factura
+    doc = minidom.parse(direccionFactura)
 
-    '''Conseguir monto tota de factura'''
-    doc = minidom.parse(directionFactura)
-    monto = doc.getElementsByTagName("cbc:PayableAmount")[0]
-    montoLoop = float(monto.firstChild.data)
+    '''Conseguir monto total de factura'''
+    montoLoop = float(conseguir_dato_segun_etiqueta(doc, direccionFactura,"cbc:PayableAmount",0))
 
     '''Conseguir nombre del cliente'''
-    doc = minidom.parse(directionFactura)
-    empresa = doc.getElementsByTagName("cbc:RegistrationName")[1]
-    nombreEmpresa = (empresa.firstChild.data)
+    nombreEmpresa = conseguir_dato_segun_etiqueta(doc, direccionFactura, "cbc:RegistrationName",1)
 
     for nombre_producto in doc.getElementsByTagName("cbc:Description"):
         productos.append(nombre_producto.firstChild.data)
     
+    '''Corroborar si la empresa no ha sido agregada'''
     if nombreEmpresa not in listaEmpresas:
         listaEmpresas.append(nombreEmpresa)
         consumoDeEmpresas.append(montoLoop)
@@ -47,15 +50,15 @@ consumo = pandas.DataFrame(consumo_por_cliente)
 consumo.to_csv("ventas/venta_1907_2507.csv")
 
 dt_informe_productos = pandas.DataFrame(index=set(productos),columns=listaEmpresas).fillna(0)
+
 for factura in facturas:
-    directionFactura = facturasOrigen + "/" + factura
-    doc = minidom.parse(directionFactura)
-    empresa = doc.getElementsByTagName("cbc:RegistrationName")[1]
-    nombreEmpresa = (empresa.firstChild.data)
+    direccionFactura = facturasOrigen + "/" + factura
+    doc = minidom.parse(direccionFactura)
+    nombreEmpresa = conseguir_dato_segun_etiqueta(doc, direccionFactura, "cbc:RegistrationName", 1)
     i = 0
     for producto in doc.getElementsByTagName("cbc:Description"):
         nombre_producto = producto.firstChild.data
-        kg = doc.getElementsByTagName("cbc:InvoicedQuantity")[i].firstChild.data
+        kg = conseguir_dato_segun_etiqueta(doc, direccionFactura, "cbc:InvoicedQuantity",i)
         i+=1
         dt_informe_productos.loc[nombre_producto, nombreEmpresa] += float(kg)
 
